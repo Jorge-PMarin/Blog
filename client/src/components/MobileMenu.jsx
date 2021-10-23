d9fe122c0eb6d17d3e196070add5d69e9ff35fc9
Jorge <jorgeluis.sk8@hotmail.com>
2021-10-23 07:10:35 -0500

    access to modal

--- client/src/components/MobileMenu.jsx
+++ client/src/components/MobileMenu.jsx
@@ -1,4 +1,5 @@
-import React, { useRef, useContext } from 'react';
+import React, { useState, useRef, useContext } from 'react';
+import LoginModal from './LoginModal';
 import { UserContext } from '../contexts/user/userContext';
 import { Logout } from '../contexts/user/userActions';
 import { Link } from 'react-router-dom';
@@ -6,6 +7,7 @@ import { axiosInstance } from '../config';
 
 export default function MobileMenu() {
   const { user, dispatchUser } = useContext(UserContext);
+  const [isOpen, setIsOpen] = useState(false);
   const MobileMenuLinksRef = useRef();
 
   function handleClick() {
@@ -29,39 +31,49 @@ export default function MobileMenu() {
   }
 
   return (
-    <div className="mobileMenu">
-      <button className=" mobileMenu__btn" onClick={handleClick}>
-        <i className="fa fa-bars"></i>
-      </button>
-      <div className="mobileMenu__links" ref={MobileMenuLinksRef}>
-        <Link to="/" className="mobileMenu__link">
-          Home
-        </Link>
-        <Link to="/about" className="mobileMenu__link">
-          About
-        </Link>
-        <Link to="/write" className="mobileMenu__link">
-          Write
-        </Link>
+    <>
+      <LoginModal isOpen={isOpen} setIsOpen={setIsOpen} />
 
-        {!user.user ? (
-          <>
-            <Link to="/" className="mobileMenu__link">
-              Sign In
-            </Link>
-            <Link to="/register" className="mobileMenu__link">
-              Sign Up
-            </Link>
-          </>
-        ) : (
-          <>
-            <Link to="/settings" className="mobileMenu__link">Account</Link>
-            <button className="MobileMenu__logout" onClick={handleLogout}>
-              Logout
-            </button>
-          </>
-        )}
+      <div className="mobileMenu">
+        <button className=" mobileMenu__btn" onClick={handleClick}>
+          <i className="fa fa-bars"></i>
+        </button>
+        <div className="mobileMenu__links" ref={MobileMenuLinksRef}>
+          <Link to="/" className="mobileMenu__link">
+            Home
+          </Link>
+          <Link to="/about" className="mobileMenu__link">
+            About
+          </Link>
+          <Link to="/write" className="mobileMenu__link">
+            Write
+          </Link>
+
+          {!user.user ? (
+            <>
+              <Link
+                to="/"
+                className="mobileMenu__link"
+                onClick={() => setIsOpen(true)}
+              >
+                Sign In
+              </Link>
+              <Link to="/register" className="mobileMenu__link">
+                Sign Up
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link to="/settings" className="mobileMenu__link">
+                Account
+              </Link>
+              <button className="MobileMenu__logout" onClick={handleLogout}>
+                Logout
+              </button>
+            </>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
