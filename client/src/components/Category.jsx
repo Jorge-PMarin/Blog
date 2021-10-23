eb1949156caa9c8a3135333de50a726a32a36d26
Jorge <jorgeluis.sk8@hotmail.com>
2021-10-23 08:29:04 -0500

    category component refactored

--- client/src/components/Category.jsx
+++ client/src/components/Category.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 export default function Category({ categoryName }) {
-  return (
-    <Link to={`/?category=${categoryName}`} className="link">
-      <span className="category">{categoryName}</span>
-    </Link>
-  );
+  return <span className="category">{categoryName}</span>;
 }
